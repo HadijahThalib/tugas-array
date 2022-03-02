a7a4b5fe6c3647b21a667ca35a8cce80186056cc
HadijahThalib <98032254+HadijahThalib@users.noreply.github.com>
2022-03-02 13:01:40 +0700

    Add files via upload

---
+++ tugas _hadijah_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[5];
+	int ratarataumur;
+	cout<<"program menghitung rata rata umur\n";
+	cout<<"masukkan umur\n";
+	cout<<"umur ke - 1 = ";
+	cin>>a[1];
+	cout<<"umur ke - 2 = ";
+	cin>>a[2];
+	ratarataumur=(a[1]+a[2])/2;
+	cout<<"maka rata rata umur = "<<ratarataumur<<"\n";
+	return 0;
+}
+
